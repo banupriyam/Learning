3fc4376c588455c4e8eea0f0a6fe1cd9e6edf082
banupriyam <41643995+banupriyam@users.noreply.github.com>
2021-03-18 18:54:39 +0530

    Persistent storage
     Local notifications

--- Learning/PersistentTableViewController.swift
+++ Learning/PersistentTableViewController.swift
@@ -6,84 +6,121 @@
 //
 
 import UIKit
+import CoreData
 
 class PersistentTableViewController: UITableViewController {
+    
+    @IBOutlet var persistentTableView: UITableView!
+//    var names: [String] = []
+    var persons: [Person] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        title = " Students Name List "
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+      super.viewWillAppear(animated)
+      
+      
+      guard let appDelegate =
+        UIApplication.shared.delegate as? AppDelegate else {
+          return
+      }
+      
+      let managedContext =
+        appDelegate.persistentContainer.viewContext
+      
+      //2
+      let fetchRequest =
+        NSFetchRequest<NSManagedObject>(entityName: "Person")
+      
+      //3
+      do {
+        persons = try managedContext.fetch(fetchRequest) as! [Person]
+      } catch {
+        print("Could not fetch. \(error)")
+      }
     }
 
-    // MARK: - Table view data source
+    
+    
+    @IBAction func addNameAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "New Name", message: "Add a New Name", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: " Save", style: .default) { action in
+            
+            guard let textField = alert.textFields?.first,
+                  let nameToSave = textField.text else {
+                return
+            }
+            //self.name.append(nameToSave)
+            //self.persistentTableView.reloadData()
+            self.saveName(name: nameToSave)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
 
+    }
+    
+    func saveName(name: String){
+        
+        guard let appDelegate =
+           UIApplication.shared.delegate as? AppDelegate else {
+           return
+         }
+         
+         let managedContext =
+           appDelegate.persistentContainer.viewContext
+        
+        let entity =
+            NSEntityDescription.entity(forEntityName: "Person",
+                                       in: managedContext)!
+          
+          let person = NSManagedObject(entity: entity,
+                                       insertInto: managedContext) as! Person
+        
+        person.name = name
+        person.addedDate = Date()
+        do {
+            try managedContext.save()
+            persons.append(person)
+            persistentTableView.reloadData()
+          } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+          }
+    }
+    
+}
+
+extension PersistentTableViewController {
+    
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return persons.count
     }
-
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell =
+            tableView.dequeueReusableCell(withIdentifier: "PersistentTableViewCellId",
+                                          for: indexPath)
+        cell.textLabel?.text = persons[indexPath.row].name
+        cell.detailTextLabel?.text = persons[indexPath.row].addedDate?.description
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
+
